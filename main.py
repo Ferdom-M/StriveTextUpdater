d61a2b6637654cb9c658a38787d5f5cfad8d2e0d
Ferdom-M <fernando.domenech.martinez@gmail.com>
2021-07-29 22:10:51 +0200

    Almost finished. Files are read and compared using string arrays, and the new file string array is overwritten. The problem is that the starting bytes change so game crashes on boot. Next step is to copy the new file and overwrite starting from the fifth line i guess.

--- main.py
+++ main.py
@@ -1,35 +1,61 @@
 import requests
+import os
 
-# url = "github url"
-# read_data = requests.get(url).content
-# dataString = read_data.decode("utf-8")
-# lineList = dataString.splitlines()
-# print(lineList[2])
+STARTING_LINE = 4
+LINE_SKIP = 1
 
-def ReadOldFile(fileName):
-    file = open(fileName, "r")
-    line = " "
-    # Skip first lines, get to SYSTEM_CLOSE
-    for i in range(8):
-        file.readline()
-
-    while(line != ""):
-        # Header, what we want to compare
-        line = file.readline()
-        # Empty
-        line = file.readline()
-        # Content, what we want to overwrite
-        line = file.readline()
-        print(line)
-        # Empty
-        line = file.readline()
+def ReadFileOnline(region):
+    url = "https://gitcdn.link/repo/Ferdom-M/StriveTextUpdater/main/REDGame" + region + ".uexp"
+    read_data = requests.get(url).content
+    dataString = read_data.decode("utf-16 le", errors="replace")
+    newLineList = dataString.splitlines()
+    return newLineList
 
 
+def ReadFileOffline(fileName):
+    file = open(fileName, "r", encoding="utf-16 le", errors="replace")
+    lineList = file.read().splitlines()
+    return lineList
+
+def CompareFileString(oldLineList, newLineList):
+    oldLine = STARTING_LINE
+    newLine = STARTING_LINE
+    oldLineListLen = len(oldLineList)
+    newLineListLen = len(newLineList)
+    while oldLine + LINE_SKIP * 2 < oldLineListLen and newLine + LINE_SKIP * 2 < newLineListLen:
+        if oldLineList[oldLine] == newLineList[newLine]:
+            oldLine += LINE_SKIP
+            newLine += LINE_SKIP
+            if oldLineList[oldLine] != newLineList[newLine]:
+                newLineList[newLine] = oldLineList[oldLine]
+            oldLine += LINE_SKIP
+            newLine += LINE_SKIP
+        else:
+            newLine += LINE_SKIP * 2
+    return newLineList
+
+def WriteNewFile(newLineList):
+    if os.path.exists("NewREDGame.uexp"):
+        os.remove("NewREDGame.uexp")
+    file = open("NewREDGame.uexp", "x", encoding="utf-16 le")
+
+    for i in range(len(newLineList)):
+        file.write(newLineList[i] + "\n")
+
+    print("Donetes have fun luv")
 
 
-ReadOldFile("REDGame.uexp")
+newLineList = ReadFileOnline(input("Enter region code of the uexp file to update: ").upper())
+#newLineList = ReadFileOffline("REDGameESN.uexp")
+if newLineList[0] != "Not Found":
+    oldLineList = ReadFileOffline("REDGame.uexp")
 
+    newLineList = CompareFileString(oldLineList, newLineList)
+
+    WriteNewFile(newLineList)
+else:
+    print(newLineList[0])
 # Starting from line 9, odd numbers are the variable name, pair numbers the content
 # Must read two consecutive lines, the second line doesnt have anything
 # Python fucking sucks why am i using this i cant remember anything what the f
-# Que para el numero de elementos de la lista tengo que poner list.count(variable) estoy hasta los cojones
+# Que para el numero de elementos de la lista tengo que poner len(variable) estoy hasta los cojones
